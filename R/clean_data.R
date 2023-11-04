1f452326d5bac1beb551371a52475e2c4787c511
carlyleben <carly.benson@yale.edu>
2023-11-04 09:31:27 -0400

    minor formatting

--- R/clean_data.R
+++ R/clean_data.R
@@ -4,8 +4,13 @@
 library(dplyr)
 library(tidyverse)
 
-# load in stacked dataframe
+# load in dataframes
+in_exploit_data <- read.csv("rawdata/files_exploits from exploit-db.csv")
 in_cyber_data <- readRDS("data/cve.with.desc.ref.conf.rds")
+
+#=========================#
+#=== format cyber data ===#
+#=========================#
 
 # unnest the metric columnh
 current_df <- in_cyber_data %>% unnest(cols = metrics, names_sep = '.')
@@ -20,5 +25,15 @@ current_df <- current_df %>% unnest(cols = weaknesses, names_sep = '.')
 # unnest weakness descriptions
 current_df <- current_df %>% unnest(cols = weaknesses.description, names_sep = '.')
 
+# change col name to make this a little nicer
+current_df <- rename(metrics.cvssMetricV31 = metricv31)
+
 # unnest metric
-current_df <- current_df %>% unnest_wider(col = metrics.cvssMetricV31, names_sep = '.')
+current_df <- current_df %>% unnest_wider(col = metricv31, names_sep = '.')
+
+#==============================#
+#=== create exploits column ===#
+#==============================#
+
+# question -- are these linked by id column? 
+# if so, we need to do some gsubbing on current_df id column to get it to match exploits data frame
